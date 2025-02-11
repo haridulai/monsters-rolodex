134d7c37e434fae19138e8076761d3335d21713d
Hari Dulai <91739271+haridulai@users.noreply.github.com>
2025-02-11 20:54:44 +0000

    Input Search Box component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import { Component } from "react";
-
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
@@ -8,28 +6,36 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: []
+      monsters: [],
     };
   }
 
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then((response) => response.json())
-    .then((users) => {
-      this.setState(
-        () => {
-          return {monsters: users}
-        },
-        () => {
-          console.log(this.state)
-        }
-      )
-    })
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => {
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        );
+      });
   }
 
   render() {
     return (
       <div className="App">
+        <input
+          className="search-box"
+          type="search"
+          placeholder="search monsters"
+          onChange={(event) => {
+            console.log(event);
+          }}
+        />
         {this.state.monsters.map((monster) => {
           return <h1 key={monster.id}>{monster.name}</h1>;
         })}
