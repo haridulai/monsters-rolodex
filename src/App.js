68bd00f7ee5923059e856235e7555f72dd2e34f2
Hari Dulai <91739271+haridulai@users.noreply.github.com>
2025-02-15 20:37:42 +0000

    Hooks: useState

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
-import { Component } from "react";
+import { useState } from 'react';
 import "./App.css";
 import CardList from "./components/card-list/card-list.component";
 import SearchBox from "./components/search-box/search-box.component";
 
 const App = () => {
+
+  const [searchField, setSearchField] = useState('');
+
+  const onSearchChange = (event) => {
+    const searchFieldString = event.target.value.toLocaleLowerCase();
+    setSearchField(searchFieldString);
+  };
+
   return (
     <div className="App">
       <h1 className="app-title">Monsters Rolodex</h1>
-{/* 
+
       <SearchBox
         onChangeHandler={onSearchChange}
         placeholder="search monsters"
         className="search-box"
       />
-      <CardList monsters={filteredMonsters} /> */}
+      {/*  <CardList monsters={filteredMonsters} /> */}
     </div>
   );
 };
